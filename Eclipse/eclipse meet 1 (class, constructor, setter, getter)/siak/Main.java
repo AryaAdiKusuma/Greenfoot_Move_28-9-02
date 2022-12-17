bf92d8ffd33d1895c7a24ef1d3724bd1d06c8bec
AryaAdiKusuma <115007708+AryaAdiKusuma@users.noreply.github.com>
2022-12-17 21:00:31 +0800

    Add files via upload

---
+++ Eclipse/eclipse meet 1 (class, constructor, setter, getter)/siak/Main.java
@@ -0,0 +1,57 @@
+package id.ac.undiksha.siak;
+
+import id.ac.undiksha.siak.entities.*;
+public class Main {
+
+	public static void main(String[] args) {
+		Person siX = new Student();
+		Person siY = new Person("caca", "Denpasar", false);
+		Student siZ = new Student("1223", "Komputer", "Teknik Informatika", "FTK");
+		Student siA = new Student("KEKE", "BULELENG", false, "12345", "Komputer", "TIK", "FTK");
+//		Person person1 = new Person;
+//		person1.setName("ADI");
+//		person1.setAddress("Singaraja");
+//		Student std1 = new Student();
+//		std1.setName("Adi");
+//		std1.setAddress("Singaraja");
+//		std1.printAllInfo();
+//		System.out.println(std1.getAddress());
+//		System.out.println(std1.getName());
+		//overidding
+//		Student std2 = new Student(
+//				"12345",
+//				"Arya",
+//				"Gobleg",
+//				true,
+//				"Computer Science",
+//				"Informatics",
+//				"FTK"
+//				
+//		);
+//		
+//		Lecturer lect1 = new Lecturer(
+//				"772255",
+//				"Nyoman Kondesi",
+//				"Gobleg",
+//				true,
+//				"Information System",
+//				"Informatics",
+//				"FTK"
+//		);
+//		
+//		Staff staff1 = new Staff(
+//				"112233",
+//				"Nyoman Bagler",
+//				true,
+//				"Penarukan",
+//				"Unit 1"
+//		);
+//		
+////		print functions
+//		std2.printAllInfo();
+//		lect1.printAllInfo();
+//		staff1.printAllInfo();
+		
+	}
+
+}
